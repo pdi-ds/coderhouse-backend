9bf8d8fc13ddb5837e1582a7f9a1dd20f16b0198
pdi-ds <pedrodeibarlucea@gmail.com>
2022-09-30 11:26:40 -0300

    Quinto desafío

---
+++ desafios/05/ejs/app.ts
@@ -0,0 +1,17 @@
+import express = require("express");
+import { Express } from "express";
+import { AddressInfo } from "net";
+import routes from "./src/routes/Products/Products";
+const PORT: number | string = process.env.PORT || 8080;
+const app: Express = express();
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", `${__dirname}/views`);
+app.use(routes);
+const server = app.listen(PORT, () => {
+  const { port } = server.address() as AddressInfo;
+  console.log(`Server running on the port  ${port}!`);
+});
+server.on("error", (err: string) => {
+  console.log(`Error ${err}.`);
+});
